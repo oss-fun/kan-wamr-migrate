cc049c55862c98af40b564acb1864907e12bd7a5
kan-fumihito <kan_f1204@docomo.ne.jp>
2021-11-24 08:27:52 +0900

    add .wasm

--- samples/basic/build.sh
+++ samples/basic/build.sh
@@ -52,6 +52,8 @@ OUT_FILE=${i%.*}.wasm
         -Wl,--allow-undefined \
         -o ${OUT_DIR}/wasm-apps/${OUT_FILE} ${APP_SRC}
 
+cp ${OUT_DIR}/wasm-apps/${OUT_FILE} ./
+
 # /opt/wasi-sdk/bin/clang     \
 #         --target=wasm32 -O0 -z stack-size=4096 -Wl,--initial-memory=65536 \
 #         --sysroot=${WAMR_DIR}/wamr-sdk/app/libc-builtin-sysroot  \
