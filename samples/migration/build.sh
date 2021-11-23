7d1ddd6bbf9cea9f90737f8e80e3c050bbb1a146
kan-fumihito <kan_f1204@docomo.ne.jp>
2021-11-24 08:27:23 +0900

    add .wasm

--- samples/migration/build.sh
+++ samples/migration/build.sh
@@ -51,6 +51,8 @@ OUT_FILE=${i%.*}.wasm
         -Wl,--export=calculate\
         -Wl,--allow-undefined \
         -o ${OUT_DIR}/wasm-apps/${OUT_FILE} ${APP_SRC}
+        
+cp ${OUT_DIR}/wasm-apps/${OUT_FILE} ./
 
 # /opt/wasi-sdk/bin/clang     \
 #         --target=wasm32 -O0 -z stack-size=4096 -Wl,--initial-memory=65536 \
