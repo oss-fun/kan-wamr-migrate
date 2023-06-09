75de89b3e1ff8b051bdfe3b88c6f07b4c1d53475
funera1 <60760935+funera1@users.noreply.github.com>
2023-06-09 18:39:58 +0900

    Support to bulk memory opcode

--- samples/osc/build.sh
+++ samples/osc/build.sh
@@ -21,7 +21,7 @@ echo "#####################build basic project"
 cd ${CURR_DIR}
 mkdir -p cmake_build
 cd cmake_build
-cmake .. 
+cmake .. -DWAMR_BUILD_BULK_MEMORY=1
 make
 if [ $? != 0 ];then
     echo "BUILD_FAIL basic exit as $?\n"
